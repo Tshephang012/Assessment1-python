0dd70a223fa6f44be5d8247fa87a325dd44eb70a
Tshephang012 <jay.t.kekana18@gmail.com>
2025-03-13 19:30:55 +0200

    Update lesson2.py

--- lesson2.py
+++ lesson2.py
@@ -1,5 +1,3 @@
-
-
 number1=50
 number2=100
 number3=150
@@ -9,12 +7,12 @@ print(number2)
 print(number3)
 print(number4)
 
-number7= 10
+number7=10
 number8=5
 results=number7+number8
 print(number7)
 print(number8)
 print(results)
 
-myname="Tshephang"
+myname="Tshephang Kekana"
 print(myname)
