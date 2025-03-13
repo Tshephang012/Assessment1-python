db97d50211dd3a014c501d0def3cebe8899761fd
Tshephang012 <jay.t.kekana18@gmail.com>
2025-03-13 19:29:18 +0200

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,20 @@
+
+
+number1=50
+number2=100
+number3=150
+number4=200
+print(number1)
+print(number2)
+print(number3)
+print(number4)
+
+number7= 10
+number8=5
+results=number7+number8
+print(number7)
+print(number8)
+print(results)
+
+myname="Tshephang"
+print(myname)
